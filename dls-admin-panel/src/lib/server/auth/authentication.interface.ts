24624e6b4bb0094d6f5d5f1d891bea12cca66704
Paweł Stępień <pstepien@interia.pl>
2023-05-15 13:39:49 +0200

    feat: initialized project, added category, items, navbar and login panel, working on the auth

---
+++ dls-admin-panel/src/lib/server/auth/authentication.interface.ts
@@ -0,0 +1,27 @@
+export type AuthTokens = {
+    access_token?: string,
+    refresh_token?: string
+}
+
+export type FetchOptions = {
+    body?: object
+}
+
+type Error = {
+    code: number,
+    message: string
+}
+
+export type Response = {
+    data?: any,
+    auth?: AuthTokens,
+    error?: Error
+}
+
+export type User = {
+    id: string,
+    name: string,
+    email: string,
+}
+
+export interface AuthenticationResponse extends Response {}
---
+++ dls-admin-panel/src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+  let username = "";
+  let password = "";
+
+  function handleSubmit() {
+    alert(`Username: ${username}\nPassword: ${password}`);
+  }
+</script>
+
+<style>
+
+body{
+  height:100%;
+background-color: #ADD8E6
+}
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  input {
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+    border: 1px solid #ccc;
+  }
+
+  button {
+    padding: 0.5rem 1rem;
+    border-radius: 0.25rem;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+  }
+</style>
+
+<body>
+<div>
+<h1>Welcome to the Admin Page</h1>
+
+<h2>Please log in </h2>
+
+
+  <label for="username">Username:</label>
+  <input type="text" id="username" bind:value={username}>
+
+  <label for="password">Password:</label>
+  <input type="password" id="password" bind:value={password}>
+
+  <button on:click={handleSubmit}>Login</button>
+</div>
+</body>
